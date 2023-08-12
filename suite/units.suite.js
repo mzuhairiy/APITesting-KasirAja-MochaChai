4ea1edc98501aee50805ec6c2c2cd59daecfdd74
mzuhairiy <atasnamazuhair@gmail.com>
2023-08-12 20:48:42 +0700

    create .del request

--- suite/units.suite.js
+++ suite/units.suite.js
@@ -6,6 +6,8 @@ const { expect } = require("chai");
 const { getToken } = require("../test/spec/get.token.spec");
 const { createUnit } = require("../test/spec/units/create.unit.spec");
 const { updateUnit } = require("../test/spec/units/update.unit.spec");
+const exp = require("constants");
+const { resolve } = require("path");
 
 
 describe('Unit feature', () => {
@@ -18,7 +20,7 @@ describe('Unit feature', () => {
         const response = await createUnit(payload,token)
         const unitId = await response.body.data.unitId; //mengambil value dari unitId di response body
         const jsonData = JSON.parse(fs.readFileSync(jsonPath, 'utf8')); //mengambil file JSON dari folder data
-        jsonData.data.unitId = unitId; //menyimpan value unitId ke dalam file JSON
+        jsonData.data.unitId = unitId; //mengirim value unitId ke dalam file JSON
         fs.writeFileSync(jsonPath, JSON.stringify(jsonData, null, 2)) //menyimpan value unitId ke dalam file JSON
         expect((await response).status).to.equal(201);
         expect((await response).body.message).to.equal("Unit berhasil ditambahkan");
@@ -38,16 +40,27 @@ describe('Unit feature', () => {
     it('should update the unit', async() => {
         const token = await getToken()
         const payload = {
-            "name": "Kilos",
+            "name": "Kiloan",
             "description": "satuan berat kilo"
         }
         const response = await updateUnit(payload,token)
         const jsonData = JSON.parse(fs.readFileSync(jsonPath, 'utf8'))
         const name = await response.body.data.name; //mengambil value dari name di response body
-        jsonData.data.name = name; //menyimpan value name ke dalam file JSON
+        jsonData.data.name = name; //mengirim value name ke dalam file JSON
         fs.writeFileSync(jsonPath, JSON.stringify(jsonData, null, 2)) //menyimpan value name ke dalam file JSON
         expect((await response).status).to.equal(200);
-        //expect((await response).body.message).to.equal("Unit berhasil ditambahkan");
-        console.log((await response).body);
+        expect((await response).body.data.name).to.be.a('string').and.to.have.lengthOf.at.least(1, "Value should not be empty")
+        //console.log((await response).body);
+    })
+    it('should delete a unit', async() => {
+        const jsonData = JSON.parse(fs.readFileSync(jsonPath, 'utf8')) //mengambil file JSON dari folder data
+        const unitId = jsonData.data.unitId
+        const token = await getToken()
+        const response = await request(config.baseUrlKA)
+        .del(`/units/` + `${unitId}`)
+        .set("Authorization", `Bearer ${token}`)
+        expect((await response).status).equal(200);
+        expect((await response).body).to.be.an('object');
+        //console.log((await response).body)
     })
 })
