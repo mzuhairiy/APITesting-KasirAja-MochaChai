520c8c9a3352a81971144f76b5a05019ce990d58
mzuhairiy <atasnamazuhair@gmail.com>
2023-08-16 09:28:32 +0700

    add suite with simplified structure code

--- suite/suites-simplified/user.suite.js
+++ suite/suites-simplified/user.suite.js
@@ -1,49 +1,59 @@
 const request = require("supertest");
 const { expect } = require("chai");
-const fs = require("fs");
-const jsonPath = 'D:/zuhair-sanbercode/js-automation/data/data-test/user.data.json'
+// const fs = require("fs");
+// const jsonPath = 'D:/zuhair-sanbercode/js-automation/data/data-test/user.data.json'
 const config = require("../../data/config.json");
 const auth = require("../../data/data-test/auth.json");
 const userData = require("../../data/data-test/user.data.json");
 
-// Gloal var
-let email;
+// Global var
 let token;
-let UserId;
+let userId;
+let name;
 
-describe("Authentication feature", () => {
-    const response = request(config.baseUrlKA).post("/registration").send(userData);
-        it("should get status and body response", async() => {
-            email = (await response).body.data.email;
-        });
+describe("User feature", () => {
+    const response = request(config.baseUrlKA).post("/authentications").send(auth);
+    it("should get a token", async() => {
+        token = (await response).body.data.accessToken;
+    });
 
-        it("should get 201 status", async() => {
-            expect((await response).status).to.eql(201);
-        });
+    it("should create a new user", async() => {
+        const response = request(config.baseUrlKA).post("/users").send(userData).set("Authorization", `Bearer ${token}`);
+        // console.log((await response).status);
+        // console.log((await response).body);
+        userId = (await response).body.data.userId;
+        expect((await response).status).to.eql(201);
+        expect((await response).body.message).to.eql("User berhasil ditambahkan");
+    });
 
-        it("should get 'Toko berhasil didaftarkan' message and valid feedback", async() => {
-            expect((await response).body.status).to.eql("success");
-            expect((await response).body.message).to.eql("Toko berhasil didaftarkan");
-            expect((await response).body.data.name).to.be.a('string').and.to.have.lengthOf.at.least(1, "value should not be empty!")
-            expect((await response).body.data.email).to.be.a('string').and.to.have.lengthOf.at.least(1, "value should not be empty!")
-        });
+    it("should get a user", async() => {
+        const response = request(config.baseUrlKA).get(`/users/${userId}`).send(userData).set("Authorization", `Bearer ${token}`);
+        //console.log((await response).status);
+        //console.log((await response).body);
+        expect((await response).status).to.eql(200);
+        expect((await response).body.data.user.id).to.eql(userId);
+        expect((await response).body.data.user.name).to.be.a('string').and.to.have.lengthOf.at.least(1, "value should not be empty!")
+    });
 
-        it("should get the same email and name values as user.data.json -Data Driven Testing-", async() => {
-            const jsonData = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
-            expect((await response).body.data.name).to.eql(jsonData.name);
-            expect((await response).body.data.email).to.eql(jsonData.email);
-        });
+    it("should update a user", async() => {
+        const payload = { "name": "kasir baru", "email": "qwerty@exx.com" }
+        const response = request(config.baseUrlKA).put(`/users/${userId}`).send(payload).set("Authorization", `Bearer ${token}`);
+        name = (await response).body.data.name;
+        // const jsonData = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+        // const name = await response.body.data.name;
+        // jsonData.data.name = name;
+        // fs.writeFileSync(jsonPath, JSON.stringify(jsonData, null, 2))
 
-        it("should success login and get token", async() => {
-            const response = request(config.baseUrlKA).post("/authentications").send(auth);
-            token = (await response).body.data.accessToken;
-            unitId = (await response).body.data.user.id;
-            expect((await response).status).to.eql(201);
-            expect((await response).body.message).to.eql("Authentication berhasil ditambahkan");
-            expect((await response).body.data.user.name).to.be.a('string').and.to.have.lengthOf.at.least(1, "value should not be empty!")
-        });
+        expect((await response).status).to.eql(200);
+        expect((await response).body.data.name).to.eql(name);
+        expect((await response).body.data.name).to.be.a('string').and.to.have.lengthOf.at.least(1, "value should not be empty!")
+    });
 
-// describe("Login feature", async() => {
-    
-//     });
-})
+    it("should delete a user", async() => {
+        const response = request(config.baseUrlKA).del(`/users/${userId}`).set("Authorization", `Bearer ${token}`);
+        // console.log((await response).status);
+        // console.log((await response).body);
+
+        expect((await response).body.message).to.eql("User berhasil dihapus");
+    });
+});
